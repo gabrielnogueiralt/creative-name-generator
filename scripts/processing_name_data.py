93decd6ca92e39856810f43f47ad571080ccb517
Acesarsilva <accs2@cin.ufpe.br>
2022-05-04 15:46:09 -0300

    Pre-processing name data

---
+++ scripts/processing_name_data.py
@@ -0,0 +1,44 @@
+# Imports
+from lib2to3.pgen2 import token
+from collections import defaultdict
+import pandas as pd
+import unidecode
+import nltk as nltk
+from nltk.corpus import stopwords
+import numpy as np
+import spacy
+import re
+import csv
+
+# Get Data
+boys = pd.read_csv('scripts/data/boys.csv')
+girls = pd.read_csv('scripts/data/girls.csv')
+
+brute_boys = boys['meaning']
+brute_girls = girls['meaning']
+
+# Pre-processing
+simple_boys = [unidecode.unidecode(txt) for txt in brute_boys]
+simple_girls = [unidecode.unidecode(txt) for txt in brute_girls]
+
+nlp = spacy.load("pt_core_news_lg", disable=['ner', 'parser'])
+
+def cleaning(doc):
+    txt = [token.lemma_ for token in doc if not token.is_stop]
+    if len(txt) > 2:
+        return ' '.join(txt)
+
+brief_cleaning_boys = (re.sub("[^A-Za-z']+", ' ', str(row)).lower() for row in simple_boys)
+brief_cleaning_girls = (re.sub("[^A-Za-z']+", ' ', str(row)).lower() for row in simple_girls)
+
+cleaning_boys = [cleaning(doc) for doc in nlp.pipe(brief_cleaning_boys, batch_size=1000)]
+cleaning_girls = [cleaning(doc) for doc in nlp.pipe(brief_cleaning_girls, batch_size=1000)]
+
+boys['clean'] = cleaning_boys
+girls['clean'] = cleaning_girls
+
+boys.to_csv('scripts/data/boys.csv', sep='\t', encoding='utf-8')
+girls.to_csv('scripts/data/girls.csv', sep='\t', encoding='utf-8')
+
+
+
